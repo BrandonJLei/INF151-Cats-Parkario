2d3426b5569aafeab7ceb064f673201841a05590
Haoda Lu <47682671+843674256@users.noreply.github.com>
2020-12-03 15:46:50 -0800

    UpdatedArrow

--- INF151 Cats Parkario/Assets/LeftArrow.cs
+++ INF151 Cats Parkario/Assets/LeftArrow.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class LeftArrow : MonoBehaviour
 {
-    
+
     void Update()
     {
-        transform.Translate(-0.01f, 0f, 0f);
+        if (transform.position.x >= -10)
+        {
+            transform.Translate(-0.01f, 0f, 0f);
+        }
+        else 
+        {
+            transform.Translate(0f, 0f, 0f);
+        } 
         
     }
 }
